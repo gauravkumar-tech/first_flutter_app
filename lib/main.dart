e3f4597d7027bcf5cec6df281a0d3fd0c676b3fb
gauravkumar-tech <gauravkumar28051997@gmail.com>
2022-07-10 15:26:26 +0530

    day2  learn Scaffold, aapbar, and drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_app/home_page.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return(
-      MaterialApp(
-       home: Material(
-        child: Center(
-          child: Container(
-            child: Text("welcome to my first flutter app"),
-          ),
-        ),
-       ),
-      )
-    );   
+    int days = 30;
+    String name = "Gaurav";
+    return (MaterialApp(home: HomePage()));
   }
 }
